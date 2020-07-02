095af8e63ccd8e3e839e0798eb3e8b53683da607
Uilton Oliveira <uilton.oliveira@cortex-intelligence.com>
2020-07-02 11:42:18 -0300

    - Allowed any hosts

--- maratonafc3_django/settings.py
+++ maratonafc3_django/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '=tt@5jq=ta3_n#r&ki-7-7=7u$gp(m!$r-)-c(6^o6)k^=)%ke'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
